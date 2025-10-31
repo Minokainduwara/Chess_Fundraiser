c22a5bdb4e34c7912f8fe68647550a94c27be8ae
Minokainduwara <minokainduwara@gmail.com>
2025-10-31 15:32:05 +0530

    fundraising routes created

---
+++ src/routes/fundraiseRoutes.js
@@ -0,0 +1,30 @@
+const routes = (app) => {
+    //fundraiser routes
+    app.route('/fundraising')
+        .get((req, res) => {
+            // Logic to get all fundraising campaigns
+            res.send('Get all fundraising campaigns');
+        })
+        .post((req, res) => {
+            // Logic to create a new fundraising campaign
+            res.send('Create a new fundraising campaign');
+        });
+
+    
+    //fundraiser by ID routes
+    app.route('/fundraising/:fundraiserId')
+        .get((req, res) => {
+            // get a specific fundraising campaign by ID
+            res.send(`Get fundraising campaign with ID: ${req.params.fundraiserId}`);
+        })
+        .put((req, res) => {
+            // update a specific fundraising campaign by ID
+            res.send(`Update fundraising campaign with ID: ${req.params.fundraiserId}`);
+        })
+        .delete((req, res) => {
+            // delete a specific fundraising campaign by ID
+            res.send(`Delete fundraising campaign with ID: ${req.params.fundraiserId}`);
+        });
+}
+
+export default routes;
