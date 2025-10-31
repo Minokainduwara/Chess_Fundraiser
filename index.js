1cf7e314ef09c80edf0257ff44ed834236097f51
Minokainduwara <minokainduwara@gmail.com>
2025-10-31 15:31:47 +0530

    setup mongoose database connection

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+import mongoose from 'mongoose';
 import express from 'express';
-
+import routes from './src/routes/fundraiseRoutes';
 const app = express();
 const PORT = process.env.PORT || 4000;
 
@@ -10,3 +11,17 @@ app.get('/', (req, res) =>
 app.listen(PORT, () => 
     console.log(`Server is running on http://localhost:${PORT}`)
 );
+
+routes(app);
+
+
+//mongoose connection
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/fundraiserdatabase', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+app.use(bodyParseer.urlencoded({ extended: true }));
+app.use(express.json());
