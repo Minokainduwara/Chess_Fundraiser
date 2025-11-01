e90e7f3f654631ec25f7f46cb9c2f70669adb8d8
Minokainduwara <minokainduwara@gmail.com>
2025-11-01 15:55:33 +0530

    Fund raiser schema created

--- src/models/fundraiserModel.js
+++ src/models/fundraiserModel.js
@@ -1,3 +1,25 @@
-import mpngoose from 'mongoose';
+import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
+export const FundraiserSchema = new Schema({
+
+    organizationName: {
+        type: String,
+        required: 'Enter the organization name'
+    },
+
+    RupeesAmount: {
+        type: Number,
+        required: 'Enter the amount needed'
+    },
+
+    comment : {
+        type: String
+    },
+
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+
+})
